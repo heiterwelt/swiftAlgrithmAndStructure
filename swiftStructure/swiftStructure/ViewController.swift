ce48840a4348d7ba46374b3c1b65710014558f85
1361579531@qq.com <1361579531@qq.com>
2016-05-03 11:35:25 +0800

    init

---
+++ swiftStructure/swiftStructure/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftStructure
+//
+//  Created by Linyou-IOS-1 on 16/5/3.
+//  Copyright © 2016年 MuYou INTERACTIVE TECHNOLOGY LIMITED. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
